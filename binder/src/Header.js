4c426cb8716e07ceb1d23f58fc3abc7209523184
bec <becsun@icloud.com>
2021-07-28 18:35:03 +0100

    button effect added

--- binder/src/Header.js
+++ binder/src/Header.js
@@ -2,14 +2,19 @@ import React from 'react';
 import "./Header.css";
 import PetsIcon from '@material-ui/icons/Pets';
 import ChatIcon from '@material-ui/icons/Chat';
+import IconButton from '@material-ui/core/IconButton';
 
 const Header = () => {
     return(
         <div className="header">
-            <PetsIcon />
+            <IconButton>
+            <PetsIcon className="header__icon" fontSize="large"/>
+            </IconButton>
             <img 
             className="header__logo" src="https://media.istockphoto.com/vectors/hare-hug-vector-id472279927?k=6&m=472279927&s=612x612&w=0&h=9_n2H93UVE2sWta-frQHsxGc2vWgf_qHQcP4QhPITZo=" alt="bunny pair" />
-            <ChatIcon />
+            <IconButton>
+            <ChatIcon className="header__icon" fontSize="large"/>
+            </IconButton>
         </div>
     )
 }
