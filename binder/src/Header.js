d43868d40bb122247b8302973fe506168e5545b4
bec <becsun@icloud.com>
2021-07-28 18:20:55 +0100

    bunny logo and added

--- binder/src/Header.js
+++ binder/src/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import "./Header.css";
 import PetsIcon from '@material-ui/icons/Pets';
 import ChatIcon from '@material-ui/icons/Chat';
 
-function Header(){
+const Header = () => {
     return(
         <div className="header">
             <PetsIcon />
-            <h1>header</h1>
+            <img 
+            className="header__logo" src="https://media.istockphoto.com/vectors/hare-hug-vector-id472279927?k=6&m=472279927&s=612x612&w=0&h=9_n2H93UVE2sWta-frQHsxGc2vWgf_qHQcP4QhPITZo=" alt="bunny pair" />
             <ChatIcon />
         </div>
     )
